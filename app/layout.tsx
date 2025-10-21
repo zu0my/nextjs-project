fc29c176204264df7198c247b799c4d363d52308
DK <2724234483@qq.com>
2025-10-21 16:00:52 +0800

    chore: init

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,7 @@
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -24,9 +15,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <head>
+        <link rel="preload" href="/font/font.css" as="style" />
+        <link rel="stylesheet" href="/font/font.css" />
+        <noscript>
+          <link rel="stylesheet" href="/font/font.css" />
+        </noscript>
+      </head>
+      <body className={`antialiased w-screen h-screen`}>
+        <SpeedInsights />
         {children}
       </body>
     </html>
